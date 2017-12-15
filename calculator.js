739613dcb1c1814fe6a2e4b902d2c246c1d99aae
SiddharthDinkar <siddharth.dinkar@warriorlife.net>
2017-12-15 14:54:36 -0800

    added two functions
    added the squared function and the cubed function

--- calculator.js
+++ calculator.js
@@ -22,3 +22,13 @@ function p() {
 	m = Math.round(m *100);
 	d(m);
 }
+function s() {
+	e();
+	var a = document.getElementById("d").value * document.getElementById("d").value;
+	d(a);
+}
+function c() {
+	e();
+	var b = document.getElementById("d").value * document.getElementById("d").value * document.getElementById("d").value;
+	d(b);
+}
